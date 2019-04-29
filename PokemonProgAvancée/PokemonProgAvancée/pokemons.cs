62e895f3b9af88685485b944af1160a5e6575cb7
LChalan <lchalan@ensc.fr>
2019-04-29 11:13:41 +0200

    Create pokemons.cs

---
+++ PokemonProgAvancée/PokemonProgAvancée/pokemons.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonProgAvancée
+{
+    class Pokemons
+    {
+
+        private string nom { get; set; }
+        private int pv { get; set; }
+        private int puissance { get; set; }
+        private string type { get; set; }
+        private string faiblesse { get; set; }
+
+        List<Pokemons> bddpokemon;
+
+        public Pokemons(string nom, int pv, int puissance, string type, string faiblesse)
+        {
+
+        }
+    }
+}
