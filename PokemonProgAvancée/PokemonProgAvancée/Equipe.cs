e2454874175434a9d36502bda870310437919a36
lchalan <lchalan@ensc.fr>
2019-05-01 13:18:35 +0200

    debut equipe

--- PokemonProgAvancée/PokemonProgAvancée/Equipe.cs
+++ PokemonProgAvancée/PokemonProgAvancée/Equipe.cs
@@ -8,11 +8,18 @@ namespace PokemonProgAvancée
 {
     class Equipe
     {
+        public string Nom { get; set; }
+        public Pokemons Pokméon1 { get; set; }
+        public Pokemons Pokméon2 { get; set; }
+        public Pokemons Pokméon3 { get; set; }
+
         public Random alea = new Random();
 
-        List<Pokemons> bddpokemon = new List<Pokemons> { };
+        List<Equipe> equipe = new List<Equipe> { };
 
-        public Equipe ()
+        List<string> noms = new List<string> { "Red", "Leaf", "Ludwig" , "Ludvina", "Kalem", "Serena", "Florent", "Scout", "Silver", "Trovato", "Giovanni", "Lysandre", "Maladif", "Pierre", "Lem"};
+
+        public Equipe (string nom)
         {
 
         }
